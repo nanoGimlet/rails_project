00de7e696675a6759a824eb50e212da03270b4df
Gimlet <45482966+nanoGimlet@users.noreply.github.com>
2020-08-23 23:40:07 +0900

    Add hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,7 @@
 class ApplicationController < ActionController::Base
 
   def hello
-    render html: "hola, mundo!"
-  end
-
-  def goodbye
-    render html: "goodbye, world!"
+    render html: "hello, world!"
   end
 
 end
